f6343b9124c648f2175a4e0b2c4231c463935069
ncthang117 <145256301+ncthang117@users.noreply.github.com>
2023-10-19 08:23:03 +0700

    Add files via upload

---
+++ ApiOrder/ApiOrder/Controllers/OrderController.cs
@@ -0,0 +1,72 @@
+﻿using ApiOrder.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.Xml.Schema;
+
+namespace ApiOrder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        public static List<Order> Orders = new List<Order>();
+        [HttpGet]
+        public IActionResult GetAll ()
+        {
+            return Ok(Orders);
+        }
+        [HttpPost]
+        public IActionResult CreateOrder (Order order)
+        {
+            var od = new Order
+            {
+                ID = Guid.NewGuid(),
+                UserID = order.UserID,
+                ProductID = order.ProductID,
+                Quantity = order.Quantity,
+                Date = order.Date,
+                Total = order.Total,
+                Status = order.Status,
+            };
+            Orders.Add(od);
+            return Ok(new { success = true, Data = od}) ;
+        }
+        [HttpDelete("{ID}")]
+        public IActionResult DeleteOrder (string ID)
+        {
+            try
+            {
+                var order = Orders.SingleOrDefault(ct => ct.ID == Guid.Parse(ID));
+                if (order == null) { return NotFound(); }
+                if (ID!= order.ID.ToString())
+                {
+                    return BadRequest();
+                }
+                Orders.Remove(order);
+                return Ok();
+            }
+            catch { return BadRequest(); }
+        }
+        [HttpPut("{ID}")]
+        public IActionResult UpdateOrder(string ID , Order orderUpdate)
+        {
+            try
+            {
+                var order = Orders.SingleOrDefault(ct => ct.ID == Guid.Parse(ID));
+                if (order == null) { return NotFound(); }
+                if (ID != order.ID.ToString()) return BadRequest();
+                order.UserID = orderUpdate.UserID;
+                order.ProductID = orderUpdate.ProductID;
+                order.Quantity = orderUpdate.Quantity;
+                order.Status = orderUpdate.Status;
+                order.Total = orderUpdate.Total;
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ ApiOrder/ApiOrder/DTO/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiOrder.Models
+{
+    public class Order
+    {
+        public Guid ID { get; set; }
+        public int UserID { get; set; }
+        public int  ProductID { get; set; }
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; } 
+        public string Date { get; set; }
+        public string Total { get; set; }
+        public string Status { get; set; }
+    }
+}
